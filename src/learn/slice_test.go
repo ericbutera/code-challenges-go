aa4630914c909ba2a75a092bfcd07900dce283da
Eric Butera <eric@ericbutera.com>
2022-08-22 20:49:02 -0400

    test: add more example slices

--- src/learn/slice_test.go
+++ src/learn/slice_test.go
@@ -20,3 +20,32 @@ func TestSlice(t *testing.T) {
 	assert.ElementsMatch(t, []string{"a", "b"}, keys)
 	assert.ElementsMatch(t, []string{"apple", "banana"}, vals)
 }
+
+func TestAppend(t *testing.T) {
+	var a []int
+	a = append(a, 1)
+	assert.Equal(t, []int{1}, a)
+	a = append(a, 2, 3, 4)
+	assert.Equal(t, []int{1, 2, 3, 4}, a)
+}
+
+func TestAppendLiteral(t *testing.T) {
+	data := []int{1}
+	data = append(data, 1)
+	assert.Equal(t, []int{1, 1}, data)
+}
+
+func TestSliceOperator(t *testing.T) {
+	bikes := []string{"yeti", "bmc", "cinelli"}
+	assert.Equal(t, []string{"yeti"}, bikes[:1]) // omit first = 0
+	assert.Equal(t, []string{"yeti", "bmc"}, bikes[0:2])
+	assert.Equal(t, []string{"bmc"}, bikes[1:2])
+	assert.Equal(t, []string{"bmc", "cinelli"}, bikes[1:3])
+	assert.Equal(t, []string{"cinelli"}, bikes[2:]) // omit last = len(bikes)
+}
+
+func TestStringSlice(t *testing.T) {
+	s := "z̵̼̩̩̿a̴͍̤͍̓́l̷̬̯̓͐ǧ̶͓̫̊̓ͅo̵̧̒͛"
+	r := []rune(s)
+	assert.Equal(t, []rune{'z', '̵', '̿', '̼', '̩', '̩', 'a', '̴', '̓', '́', '͍', '̤', '͍', 'l', '̷', '̓', '͐', '̬', '̯', 'g', '̶', '̌', '̊', '̓', '͓', 'ͅ', '̫', 'o', '̵', '̒', '͛', '̧'}, r)
+}
